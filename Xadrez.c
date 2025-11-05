1db5b35602281a046b5c1cf23c9356b65f929fbb
andreleopoldosf-debug <andreleopoldo.sf@gmail.com>
2025-11-05 23:42:33 +0000

    Xadrez

---
+++ Xadrez.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    
+    int numero;
+
+    numero = 30;
+    
+
+    if(numero %2 == 0) {
+      printf("Número digitado é par\n");
+    } else {
+      printf("Número digitado é impar\n");
+      }
+    
+
+    return 0;
+}
